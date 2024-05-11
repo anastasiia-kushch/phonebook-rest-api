78f89c662fb80811bc414569c52b11b2213562dc
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-05-11 17:57:42 +0200

    start project

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,104 @@
+import Contact from '../db/models/Contact.js';
+import { isValidObjectId } from 'mongoose';
+import HttpError from '../helpers/HttpError.js';
+
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await Contact.findById(id);
+
+    if (contact === null) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const contact = await Contact.findByIdAndDelete(id);
+
+    if (contact === null) {
+      return res
+        .status(404)
+        .json({ message: 'Contact not found or already deleted' });
+    }
+    res.status(200).json(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+
+  try {
+    const result = await Contact.create(contact);
+    console.log(result);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  const { id } = req.params;
+
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+  };
+
+  try {
+    const result = await Contact.findByIdAndUpdate(id, contact, { new: true });
+
+    if (result === null) {
+      return res.status(404).json({ message: 'Contact not found' });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateStatusContact = async (req, res, next) => {
+  const { id } = req.params;
+  const { favourite } = req.body;
+
+  try {
+    if (!isValidObjectId(id)) {
+      throw HttpError(404, `"message":"Not found"`);
+    }
+
+    const result = await Contact.findByIdAndUpdate(
+      id,
+      { favourite },
+      { new: true }
+    );
+    if(!result) {
+      throw HttpError(404);
+    }
+
+    res.status(200).json(result)
+  } catch (error) {
+    next(error);
+  }
+};

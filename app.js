d74b981efa6e06a40159bffa81aa7cd5f8711513
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-05-13 11:35:45 +0200

    fix app

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use('/api/contacts', contactsRouter);
 
-app.use((_, res) => {
+app.use((req, res, next) => {
   res.status(404).json({ message: 'Route not found' });
 });
 

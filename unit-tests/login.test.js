609b4c838174f91bbc0b5e4b0c7c6bda60bcd8c3
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-05-19 17:43:35 +0200

    edit tests

--- unit-tests/login.test.js
+++ unit-tests/login.test.js
@@ -1,37 +1,33 @@
-//відповідь повина мати:
-// • статус-код 200
-// • токен
-// • об'єкт user з 2 полями email и subscription з типом даних String
-import jest from '@jest/globals'
+import { jest } from '@jest/globals';
 
 import supertest from 'supertest';
 import app from '../app.js';
 import User from '../db/models/User.js';
 
-jest.mock('../db/models/User.js')
+jest.mock('../db/models/User.js');
 
-describe('login', () => {
+describe('POST /api/users/login', () => {
   test('should return status code 200, token, and user object with email and subscription fields', async () => {
     const mockedUser = {
       _id: 'userId',
       email: 'email@mail.com',
-      password: '00000000',
+      password: '$2b$10$2TE.YoP9G1jPMuxRJmJH7e2b.Xjsk5cZ0eK2nU6jXf9zEJ7lM36qW',
       token: 'fakeToken',
-      subscription: 'pro',
+      subscription: 'free',
     };
 
-    await User.findOne.mockResolveVajue(mockedUser);
+    User.findOne.mockResolvedValue({ mockedUser });
 
-    const response = await supertest(app)
-      .post('/api/users/login')
-      .send({ email: 'email@mail.com', password: '00000000' })
-      .set('Accept', 'application/json')
-      .expect('Content-Type', /json/)
-      .expect(200);
+    const response = await supertest(app).post('/api/users/login').send({
+      email: 'email@mail.com',
+      password: '00000000',
+    });
 
-    exprect(response.body).toHaveProperty('token');
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+    expect(response.body.user).toBeDefined();
     expect(response.body.user.email).toBe('email@mail.com');
-    expect(response.body.user.subscription).toBe('pro');
+    expect(response.body.user.subscription).toBe('free');
     expect(typeof response.body.user.email).toBe('string');
     expect(typeof response.body.user.subscription).toBe('string');
   });

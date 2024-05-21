c9600f82e623331f35b0b2131d98e46c9fa233e1
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-05-21 13:07:14 +0200

    editing tests

--- unit-tests/login.test.js
+++ unit-tests/login.test.js
@@ -7,24 +7,26 @@ jest.mock('../db/models/User.js');
 
 describe('POST /api/users/login', () => {
   test('should return status code 200, token, and user object with email and subscription fields', async () => {
-    const mockedUser = {
-      _id: '6648824cd28880abe7be0fec',
-      email: 'email@mail.com',
-      password: '$2b$10$/c0a1O3dOhN.mWg.TZb73ev76YHIx08C0T22JN5kMgCmEfeOe4rnG',
-      token: 'fakeToken',
-      subscription: 'starter',
-    };
+    // const mockedUser = {
+    //   _id: "userId",
+    //   email: "email@mail.com",
+    //   password: "$2b$10$2TE.YoP9G1jPMuxRJmJH7e2b.Xjsk5cZ0eK2nU6jXf9zEJ7lM36qW",
+    //   token: "fakeToken",
+    //   subscription: "pro",
+    // };
 
     const response = await supertest(app).post('/api/users/login').send({
-      email: 'email@mail.com',
-      password: '00000000',
+      email: 'User1@mail.com',
+      password: '123456',
     });
-    console.log(response.body);
+
+    console.log('response', response.body);
     expect(response.status).toBe(200);
     expect(response.body).toHaveProperty('token');
-    expect(response.body.email).toBe('email@mail.com');
-    expect(response.body.subscription).toBe('starter');
-    expect(typeof response.body.email).toBe('string');
-    expect(typeof response.body.subscription).toBe('string');
+    expect(response.body.user).toBeDefined();
+    expect(response.body.user.email).toBe('User1@mail.com');
+    // expect(response.body.user.subscription).toBe("pro");
+    expect(typeof response.body.user.email).toBe('string');
+    // expect(typeof response.body.user.subscription).toBe("string");
   });
 });
